924952716bae5c255e567233cd24db20d489a7ab
Dmitry Muhomor <muhomor.dmitry@gmail.com>
2023-05-16 18:15:39 +0300

    cscopes: add ScopedContactsProvider
    See ContactScopes.md for documentation
    
    Change-Id: Iebb872d0c8eb50f98b98ece5976394515438a83c

--- src/com/android/providers/contacts/ContactsProvider2.java
+++ src/com/android/providers/contacts/ContactsProvider2.java
@@ -5902,7 +5902,7 @@ public class ContactsProvider2 extends AbstractContactsProvider
         }
     }
 
-    private static Cursor createEmptyCursor(final Uri uri, String[] projection) {
+    static Cursor createEmptyCursor(final Uri uri, String[] projection) {
         projection = projection == null ? getDefaultProjection(uri) : projection;
         if (projection == null) {
             return null;
